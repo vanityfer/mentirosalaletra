3d36ebdd6aa34f997408b65577ff7540fa90a091
vanityfer <75648982+vanityfer@users.noreply.github.com>
2022-02-05 00:04:55 -0300

    Update dosCanvas.js

--- dosCanvas.js
+++ dosCanvas.js
@@ -1,5 +1,5 @@
 function preload() {
-  inconsolata = loadFont('assets/Inconsolata.ttf');
+  inconsolata = loadFont('assets/inconsolata.ttf');
 }
 
 var s1 = function( sketch ) {
@@ -8,7 +8,7 @@ var s1 = function( sketch ) {
    
     let canvas1 = sketch.createCanvas(sketch.windowWidth/3, sketch.windowHeight/3);
     canvas1.position(sketch.windowWidth/3,0);
-    inconsolata = sketch.loadFont('assets/Inconsolata.ttf');
+    inconsolata = sketch.loadFont('assets/inconsolata.ttf');
     sketch.textFont(inconsolata);
     sketch.textSize(sketch.width / 10);
    // sketch.textAlign(sketch.right, sketch.right);
@@ -66,31 +66,36 @@ var s1 = function( sketch ) {
 // create a new instance of p5 and pass in the function for sketch 1
 new p5(s1);
 
-var s2 = function( sketch ) {
+var s2 = function( sketch2 ) {
   
-   sketch.setup = function() {
-    let canvas2 = sketch.createCanvas(sketch.windowWidth/3, sketch.windowHeight/3, sketch.WEBGL);
+   sketch2.setup = function() {
+    let canvas2 = sketch2.createCanvas(sketch2.windowWidth/3, sketch2.windowHeight/3, sketch2.WEBGL);
     canvas2.position(-50,25);
     src = 'assets/images/' + Math.floor( Math.random() * 20 ) + ".png";
-    img = sketch.loadImage(src);
+    img = sketch2.loadImage(src);
     theta = 0;
   }
-  sketch.draw = function() {
+  sketch2.draw = function() {
     //for canvas 2
     
     
-    sketch.translate(0, 0, 0);
-    sketch.push();
-    sketch.rotateZ(theta * 0.1);
-    sketch.rotateX(theta * 0.1);
-    sketch.rotateY(theta * 0.1);
+    sketch2.translate(0, 0, 0);
+    sketch2.push();
+    sketch2.rotateZ(theta * 0.1);
+    sketch2.rotateX(theta * 0.1);
+    sketch2.rotateY(theta * 0.1);
     
-    sketch.texture(img);
-    sketch.box(100, 100, 100);
-    sketch.pop();
+    sketch2.texture(img);
+    sketch2.box(100, 100, 100);
+    sketch2.pop();
     theta += 0.05;
   
   }
+  sketch2.mouseClicked = function () {
+    new p5(s2);
+    
+    }
+  
   
 };
 
@@ -101,7 +106,7 @@ var s3 = function( sketch ) {
   sketch.setup = function() {
     let canvas3 = sketch.createCanvas(sketch.windowWidth/3, sketch.windowHeight/3);
     canvas3.position(sketch.windowWidth/3,sketch.windowHeight/3);
-    inconsolata = sketch.loadFont('assets/Inconsolata.ttf');
+    inconsolata = sketch.loadFont('assets/inconsolata.ttf');
     sketch.textFont(inconsolata);
     sketch.textSize(sketch.width /10);
     sketch.textAlign(sketch.right, sketch.right);
@@ -186,7 +191,7 @@ var s4 = function( sketch ) {
    sketch.rotateY(theta * 0.1);
    
    sketch.texture(img1);
-   sketch.box(85, 85, 85);
+   sketch.box(100, 100, 100);
    sketch.pop();
    theta += 0.05;
  
@@ -199,6 +204,46 @@ var s4 = function( sketch ) {
 new p5(s4);
 
 
+var s5 = function( sketch ) {
+
+  sketch.setup = function() {
+   let canvas5 = sketch.createCanvas(sketch.windowWidth/3, sketch.windowHeight/3, sketch.WEBGL);
+   canvas5.position(100,sketch.windowHeight-(sketch.windowHeight/3));
+   numero = Math.floor( Math.random() * 20 )
+   if (numero>20){
+     numero = 0;
+   } else {
+     numero = numero+2
+   }
+   src2 = 'assets/images/' + numero + ".png";
+   img2 = sketch.loadImage(src1);
+   theta = 0;
+ }
+ sketch.draw = function() {
+   //for canvas 2
+   
+   
+   sketch.translate(0, 0, 0);
+   sketch.push();
+   sketch.rotateZ(theta * 0.1);
+   sketch.rotateX(theta * 0.1);
+   sketch.rotateY(theta * 0.1);
+   
+   sketch.texture(img2);
+   sketch.box(70, 70, 70);
+   sketch.pop();
+   theta += 0.05;
+ 
+ }
+
+ 
+};
+
+// create the second instance of p5 and pass in the function for sketch 2
+new p5(s5);
+
+
+// 
 var s5 = function( sketch ) {
 
   sketch.setup = function() {
@@ -238,7 +283,7 @@ var s5 = function( sketch ) {
 new p5(s5);
 
 
-// 
+
 
 var hydra = new Hydra({
   canvas: document.getElementById("myCanvas")
@@ -259,7 +304,7 @@ function setup() {
   hc = select("#myCanvas")
   hc.hide()
   
-  pg = createGraphics(width, height)
+  pg = createGraphics(100,100);
 }
 
 function draw() {
@@ -277,13 +322,10 @@ function draw() {
     fill(pg.pixels[id+0],pg.pixels[id+1],pg.pixels[id+2])
     //fill(pg.get(x, y))
     ellipse(x, y, 5);
+    if (frameCount == 1100){
+      window.location.reload();
+    }
   }
 }
-
-
-
-
-
-
 
 
